81f16ff52854cce6a37e985f3c2d57c039fcca6f
Hamza <software22w@gmail.com>
2024-02-23 23:37:28 +0200

    Finish sebha and radio

--- lib/moduls/radio/page/radio_view.dart
+++ lib/moduls/radio/page/radio_view.dart
@@ -5,11 +5,46 @@ class radioView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text("Radio page",
-            style: TextStyle(fontSize: 40, fontFamily: "El Meseiri")),
-      ),
+    var mediaQuery = MediaQuery.of(context).size;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: mediaQuery.height * 0.1),
+              child: Image.asset("assets/images/radio-vector-png.png",
+                  height: mediaQuery.height * 0.3, width: mediaQuery.width),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "إذاعة القرآن الكريم",
+          style: TextStyle(
+              fontFamily: "El Meseiri",
+              fontSize: 25,
+              fontWeight: FontWeight.w500),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          children: [
+            Spacer(flex: 5),
+            Image.asset("assets/images/Icon metro-nextt.png"),
+            Spacer(flex: 2),
+            Image.asset("assets/images/Icon awesome-play.png"),
+            Spacer(flex: 1),
+            Image.asset("assets/images/Icon metro-next.png",
+                width: mediaQuery.width * 0.2),
+            Spacer(flex: 3),
+          ],
+        )
+      ],
     );
   }
 }
--- lib/moduls/tasbeh/page/tasbeh_view.dart
+++ lib/moduls/tasbeh/page/tasbeh_view.dart
@@ -1,15 +1,106 @@
 import 'package:flutter/material.dart';
 
-class TasbehView extends StatelessWidget {
+class TasbehView extends StatefulWidget {
   const TasbehView({super.key});
 
   @override
+  State<TasbehView> createState() => _TasbehViewState();
+}
+
+class _TasbehViewState extends State<TasbehView> {
+  int counter = 0;
+  int index = 0;
+  double angle = 0;
+  List<String> tasbehList = [
+    "سبحان الله",
+    "الحمد لله",
+    "لا اله الا الله",
+    "الله اكبر"
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text("Tasbeh page",
-            style: TextStyle(fontSize: 40, fontFamily: "El Meseiri")),
+    var mediaQuery = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: mediaQuery.height * 0.07),
+                child: Transform.rotate(
+                    angle: angle,
+                    child: Image.asset("assets/images/body of seb7a.png")),
+              ),
+              Image.asset("assets/images/head of seb7a.png"),
+            ],
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+            "عدد التسبيحات",
+            style: TextStyle(
+                fontFamily: "El Meseiri",
+                fontSize: 25,
+                fontWeight: FontWeight.w500),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Container(
+            padding: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(35),
+              color: Color(0xffB7935F),
+            ),
+            child: Text(
+              "$counter",
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          InkWell(
+            onTap: () {
+              tasbeh();
+            },
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(35),
+                color: Color(0xffB7935F),
+              ),
+              child: Text(
+                tasbehList[index],
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.normal),
+              ),
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  void tasbeh() {
+    if (counter < 33) {
+      angle += 1;
+      counter++;
+    } else {
+      if (index < 3) {
+        index++;
+        counter = 0;
+      } else {
+        index = 0;
+        counter = 0;
+      }
+    }
+    setState(() {});
   }
 }
